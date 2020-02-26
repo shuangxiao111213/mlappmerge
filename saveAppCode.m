4bedd58ad51313e59849f235a55f47a3fbff0f22
Shuang Xiao <shuangx@mathworks.com>
2020-02-26 14:08:16 -0500

    upload m

---
+++ saveAppCode.m
@@ -0,0 +1,26 @@
+function [loadOutcome, loadedData] = saveAppCode(filepath, code)   
+    % Assume load will be successful
+    loadOutcome.Status = 'success';
+    loadedData = struct.empty;
+
+   import appdesigner.internal.serialization.validator.deserialization.*;
+           validators = {...
+            
+        ... Data Integrity
+        MLAPPReleaseValidator, ...
+        MLAPPTypeValidator, ...
+        MLAPPResponsiveAppValidator ...
+        ... Environment
+        MLAPPLicenseValidator ...
+        
+        };
+     % create a deserializer and get the app Data
+    deserializer = appdesigner.internal.serialization.MLAPPDeserializer(filepath, validators);
+    loadedData = deserializer.getAppData();
+    
+    
+    fileWriter = appdesigner.internal.serialization.FileWriter(filepath);
+
+    fileWriter.writeMATLABCodeText(code);
+    loadOutcome.Status ='updated';
+end
